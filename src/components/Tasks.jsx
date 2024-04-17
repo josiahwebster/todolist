1fe88e15cc9e30f5050321f00cfdde755cde893b
josiahwebster <josiahwebster12@gmail.com>
2024-04-17 11:47:19 +1000

    todolist table + map

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import taskList from '../../tasks.json'
-console.log(taskList);
+import React from 'react';
+import taskList from '../../tasks.json';
 
 const Tasks = () => {
   return (
-   
+
     <div>
-
         <h1>Welcome to your ToDoList</h1>
-        <form>
-            <input type="text" placeholder="Enter task" />
-            <button type="submit">Add Task</button>
-        </form>
-      
-    </div>
-  )
-}
+        <ul>
+        {taskList.map((task,index) => {
 
-export default Tasks
+        return(    
+            <>
+            <li>
+                <input type="checkbox" id="task-1" />
+                <label htmlFor="task-1">{task.title}</label><hr />
+                <label htmlFor="task-2">{task.description}</label>
+              </li>
+
+              </>)
+
+
+
+    })}
+    <form>
+        <input type="text" placeholder="Enter task" />
+        <button type="submit">Add Task</button>
+      </form>
+
+    </ul>
+    </div>
+  
+  );
+};
+
+export default Tasks;
